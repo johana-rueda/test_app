7bbdb5e217ae13ecab88ef315e61c71d938388e5
johana-rueda <johana.m.rueda@gmail.com>
2020-07-06 19:42:35 -0500

    adding new and index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,16 @@
 class ArticlesController < ApplicationController
     def show
-        byebug
         @article = Article.find(params[:id])
+    end
+
+    def index
+        @articles = Article.all 
+    end
+
+    def new 
+    end
+
+    def create
     end
 
 end
