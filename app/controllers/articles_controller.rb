6c37a12064fc41e8e0cc32390f83d809cd938e1b
johana-rueda <johana.m.rueda@gmail.com>
2020-07-07 18:11:50 -0500

    Code cleanup

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,18 +1,9 @@
 class ArticlesController < ApplicationController
-    def show
-        @article = Article.find(params[:id])
-    end
 
-    def index
-        @articles = Article.all 
-    end
-
-    def new
-        @article = Article.new
-    end
-
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
+    
     def create
-        @article = Article.new(params.require(:article).permit(:title, :description))
+        @article = Article.new(article_params)
         if @article.save
             flash[:notice]= "Article was created successfully"
           redirect_to @article 
@@ -20,14 +11,28 @@ class ArticlesController < ApplicationController
           render 'new'
         end
     end
+    
+    def destroy
+        @article.destroy
+        redirect_to articles_path
+    end
 
     def edit
-        @article = Article.find(params[:id])
+    end
+
+    def index
+        @articles = Article.all 
+    end
+    
+    def new
+        @article = Article.new
+    end
+
+    def show
     end
 
     def update
-        @article = Article.find(params[:id])
-        if @article.update(params.require(:article).permit(:title, :description))
+        if @article.update(article_params)
             flash[:notice]= "Article was updated successfully"
             redirect_to @article 
         else
@@ -35,10 +40,13 @@ class ArticlesController < ApplicationController
         end
     end
 
-    def destroy
+    private
+    
+    def set_article
         @article = Article.find(params[:id])
-        @article.destroy
-        redirect_to articles_path
     end
 
+    def article_params
+        params.require(:article).permit(:title, :description)
+    end
 end
